870facaa51cdb0b9a8981acdc7d95fb78711eda9
Naufalhff <naufalhidayatul01@gmail.com>
2024-09-25 10:30:39 +0700

    update

--- User/frontend/src/router/index.js
+++ User/frontend/src/router/index.js
@@ -6,10 +6,10 @@ import AboutPage from "../views/AboutPage.vue";
 import DonationPage from "../views/DonationPage.vue";
 import EventsPage from "../views/EventPage.vue";
 import LoginPage from "@/views/LoginPage.vue";
-import ForgotPassword from "@/views/ForgotPassword.vue"; 
-import VerifyCode from '@/views/VerifyCode.vue';
-import ResetPassword from '@/views/ResetPassword.vue';
-
+import ForgotPassword from "@/views/ForgotPassword.vue";
+import VerifyCode from "@/views/VerifyCode.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
+import RegisterPage from "../views/RegisterPage.vue";
 
 const routes = [
   {
@@ -40,15 +40,21 @@ const routes = [
     meta: { hideNavbar: true },
   },
   {
-    path: '/verify-code',
-    name: 'VerifyCode',
+    path: "/verify-code",
+    name: "VerifyCode",
     component: VerifyCode,
     meta: { hideNavbar: true },
   },
   {
-    path: '/reset-password',
-    name: 'ResetPassword',
+    path: "/reset-password",
+    name: "ResetPassword",
     component: ResetPassword,
+    meta: { hideNavbar: true },
+  },
+  {
+    path: "/register",
+    name: "RegisterPage",
+    component: RegisterPage,
     meta: { hideNavbar: true },
   },
 ];
