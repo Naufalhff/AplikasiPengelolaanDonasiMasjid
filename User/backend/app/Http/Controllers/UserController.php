0300da44092bf7e72ba402b89484adab892dada5
Firlyansyah <undeadcatgirl5@gmail.com>
2024-10-02 08:19:34 +0700

    Fix error si ariq

--- User/backend/app/Http/Controllers/UserController.php
+++ User/backend/app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 use App\Mail\OTPMail;
 
 class UserController extends Controller
