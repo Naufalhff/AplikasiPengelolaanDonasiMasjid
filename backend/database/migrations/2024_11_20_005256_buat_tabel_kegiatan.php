d10c9cfb4242ad15a8c91aaf6c9cf53cfd3fcf4f
Naufalhff <naufalhidayatul01@gmail.com>
2024-12-06 18:31:45 +0700

    update jenis zakat

--- backend/database/migrations/2024_11_20_005256_buat_tabel_kegiatan.php
+++ backend/database/migrations/2024_11_20_005256_buat_tabel_kegiatan.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up()
     {
         Schema::create('kegiatan', function (Blueprint $table) {
             $table->id('id_kegiatan');
             $table->string('foto_kegiatan', 255);
             $table->string('nama_kegiatan', 100)->unique();
-            $table->enum('jenis_kegiatan', ['ZAKAT', 'SUMBANGAN', 'ZAKAT MAL']);
+            $table->enum('jenis_kegiatan', ['ZAKAT FITRAH', 'SUMBANGAN', 'JENIS ZAKAT MAL', 'ZAKAT MAL']);
             $table->text('deskripsi_kegiatan');
             $table->decimal('anggaran_donasi', 15, 2);
             $table->decimal('anggaran_terkumpul', 15, 2);
--- backend/database/seeders/SeederKegiatan.php
+++ backend/database/seeders/SeederKegiatan.php
@@ -13,7 +13,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah.jpg',
                 'nama_kegiatan' => 'Zakat Fitrah',
-                'jenis_kegiatan' => 'ZAKAT',
+                'jenis_kegiatan' => 'ZAKAT FITRAH',
                 'deskripsi_kegiatan' => 'Zakat Fitrah adalah zakat yang wajib dikeluarkan oleh umat Muslim pada bulan Ramadan sebagai bentuk penyucian diri dan solidaritas kepada mereka yang membutuhkan.',
                 'anggaran_donasi' => 5000000.00,
                 'anggaran_terkumpul' => 3000000.00,
@@ -37,7 +37,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_mal.jpg',
                 'nama_kegiatan' => 'Zakat Mal',
-                'jenis_kegiatan' => 'ZAKAT',
+                'jenis_kegiatan' => 'ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat Mal adalah zakat harta yang dikeluarkan oleh umat Muslim atas kekayaan yang dimiliki, seperti emas, perak, uang, dan aset lainnya, untuk membantu mereka yang membutuhkan.',
                 'anggaran_donasi' => 7000000.00,
                 'anggaran_terkumpul' => 5000000.00,
@@ -73,7 +73,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_emas.jpg',
                 'nama_kegiatan' => 'Zakat Emas dan Logam',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat emas dan logam merupakan zakat yang dikenakan atas kepemilikan emas dan logam mulia lainnya yang telah mencapai nisab dan haul.',
                 'anggaran_donasi' => 8000000.00,
                 'anggaran_terkumpul' => 6000000.00,
@@ -109,7 +109,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Perusahaan',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat perusahaan adalah zakat yang dikenakan atas keuntungan perusahaan yang telah mencapai nisab selama satu tahun.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -121,7 +121,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Perdagangan',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat perdagangan dikenakan atas harta perdagangan seperti barang dagangan yang mencapai nisab dan haul.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -133,7 +133,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Tabungan dan Investasi',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat tabungan dan investasi merupakan zakat yang dikenakan atas harta simpanan dan investasi yang telah mencapai nisab.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -145,7 +145,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Pertanian',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat pertanian adalah zakat yang dikeluarkan dari hasil panen pertanian setelah mencapai nisab.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -157,7 +157,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Peternakan',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat peternakan dikenakan atas hasil peternakan yang mencapai nisab, seperti kambing, sapi, dan unta.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -169,7 +169,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Pertambangan',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat pertambangan adalah zakat yang dikeluarkan dari hasil pertambangan yang mencapai nisab.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -181,7 +181,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Rikaz',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat rikaz adalah zakat yang dikenakan atas harta karun atau barang temuan yang ditemukan.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
@@ -193,7 +193,7 @@ class SeederKegiatan extends Seeder
             [
                 'foto_kegiatan' => 'zakat_fitrah3.jpg',
                 'nama_kegiatan' => 'Zakat Penghasilan',
-                'jenis_kegiatan' => 'ZAKAT MAL',
+                'jenis_kegiatan' => 'JENIS ZAKAT MAL',
                 'deskripsi_kegiatan' => 'Zakat penghasilan adalah zakat yang dikenakan atas penghasilan atau gaji yang telah mencapai nisab.',
                 'anggaran_donasi' => 11000000.00,
                 'anggaran_terkumpul' => 9000000.00,
